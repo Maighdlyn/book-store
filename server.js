120c83773a657a0a8d039f528c53b37a135b1a96
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-14 10:48:32 -0700

    Server.js is now rendering simple text on the home page

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express')
 const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/src/views')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Express server running on port ${port}.`)
+})
